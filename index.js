588b8f8a3b8300bff2fa725fde21390a4fb54195
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-06-19 22:08:55 +0200

    .

--- index.js
+++ index.js
@@ -10,17 +10,25 @@ app.get('/', (req, res) => {
     }).catch(console.error)
 })
 
+app.get('/:year', (req, res) => {
+    run(req.params.year).then(v => {
+        res.render('index', { v, year: req.params.year })
+    }).catch(console.error)
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
 const uri = `mongodb+srv://admin:${process.env.PASSWORD}@sob.w9c6jzn.mongodb.net/?retryWrites=true&w=majority&appName=sob`;
 const client = new MongoClient(uri, {serverApi: {version: ServerApiVersion.v1}})
-async function run() {
+async function run(year) {
     try {
+        if (!year) year = 2000
+        else year = Number(year)
         await client.connect()
         const database = client.db("sample_mflix")
         const sob = database.collection("movies")
-        const answ = await sob.find({year: 1968}).toArray()
+        const answ = await sob.find({year}).toArray()
         return answ  
     } finally {
         await client.close()
