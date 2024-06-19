22aeafa62024eb805a2e3142faeacb9e0d9c45b4
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-06-19 19:04:00 +0200

    .

---
+++ index.js
@@ -0,0 +1,26 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    run().then(v => res.send(v)).catch(console.error)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+const uri = `mongodb+srv://admin:${process.env.PASSWORD}@sob.w9c6jzn.mongodb.net/?retryWrites=true&w=majority&appName=sob`;
+const client = new MongoClient(uri, {serverApi: {version: ServerApiVersion.v1}})
+async function run() {
+    try {
+        await client.connect()
+        const database = client.db("sob")
+        const sob = database.collection("users")
+        const answ = await sob.find({name:"Jane Doe"}).toArray()
+        return answ  
+    } finally {
+        await client.close()
+    }
+}
+
